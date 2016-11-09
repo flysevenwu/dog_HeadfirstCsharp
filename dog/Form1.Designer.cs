9a5ebb3e75f60dda3d82bf476dba78d24bc84e40
WuQingzhe <wqz@seu.edu.cn>
2016-11-09 22:40:43 +0800

    head first C#

---
+++ dog/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace dog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(595, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.numericUpDown2);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(14, 257);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(598, 179);
+            this.panel1.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(344, 109);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 12);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Al";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(344, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Bob";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(344, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Joe";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(345, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "赌注内容";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(296, 150);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(91, 21);
+            this.numericUpDown2.TabIndex = 9;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(147, 151);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(64, 21);
+            this.numericUpDown1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(234, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "狗的序号";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(474, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 61);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "开始";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(66, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Bets";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Joe";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(21, 109);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(35, 16);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Al";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(21, 78);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(41, 16);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Bob";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(21, 46);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(41, 16);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Joe";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "最小下注5元";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(25, 35);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(72, 26);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(25, 91);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(72, 26);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(25, 146);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(72, 26);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(25, 195);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(72, 26);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 447);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ dog/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace dog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Point p1 = new Point(25, 35);
+        Point p2 = new Point(25, 91);
+        Point p3 = new Point(25, 149);
+        Point p4 = new Point(25, 200);
+        int winner;
+        private int Joe_a;
+        private int Bob_a;
+        private int Al_a;
+        static Guy Joe = new Guy("Joe", 50);
+        static Guy Bob = new Guy("Bob", 75);
+        static Guy Al = new Guy("Al", 45);
+        static Bet Joe_b = new Bet(Joe);
+        static Bet Bob_b = new Bet(Bob);
+        static Bet Al_b = new Bet(Al);
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            pictureBox2.Location = p1;
+            pictureBox3.Location = p2;
+            pictureBox4.Location = p3;
+            pictureBox5.Location = p4;
+            Greyhound g1 = new Greyhound(pictureBox2, 25);
+            Greyhound g2 = new Greyhound(pictureBox3, 25);
+            Greyhound g3 = new Greyhound(pictureBox4, 25);
+            Greyhound g4 = new Greyhound(pictureBox5, 25);
+            bool a1, a2, a3, a4;
+            while (true)
+            {
+                //Thread.Sleep(100);
+                a1 = g1.Run();
+                a2 = g2.Run();
+                a3 = g3.Run();
+                a4 = g4.Run();
+                if (a1 || a2 || a3 || a4)
+                {
+                    int count = 0;
+                    if (a1) count++;
+                    if (a2) count++;
+                    if (a3) count++;
+                    if (a4) count++;
+                    bool[] a = { a1, a2, a3, a4 };
+                    Greyhound[] g = { g1, g2, g3, g4 };
+                    if (count == 1)
+                    {
+                        for (int i = 0; i <= 3; i++)
+                        {
+                            if (a[i])
+                            {                               
+                                button1.Enabled = true;
+                                button2.Enabled = false;
+                                winner = i + 1;
+                                Joe_a = Joe_b.payout(winner);
+                                Bob_a = Bob_b.payout(winner);
+                                Al_a = Al_b.payout(winner);
+                                Joe.cash += Joe_a;
+                                Bob.cash += Bob_a;
+                                Al.cash += Al_a;
+                                Joe.updatelable();
+                                Bob.updatelable();
+                                Al.updatelable();
+                                Joe.ClearBet();
+                                Bob.ClearBet();
+                                Al.ClearBet();
+                                MessageBox.Show((i + 1).ToString() + "号狗赢了");
+                                break;
+                            } 
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        int c = compare(g1.mypicturebox.Location.X, g2.mypicturebox.Location.X, g3.mypicturebox.Location.X, g4.mypicturebox.Location.X);
+
+                        for (int i = 0; i <= 3; i++)
+                        {
+                            if (g[i].mypicturebox.Location.X == c)
+                            {
+                                winner = i + 1;
+                                Joe_a = Joe_b.payout(winner);
+                                Bob_a = Bob_b.payout(winner);
+                                Al_a = Al_b.payout(winner);
+                                Joe.cash += Joe_a;
+                                Bob.cash += Bob_a;
+                                Al.cash += Al_a;
+                                Joe.updatelable();
+                                Bob.updatelable();
+                                Al.updatelable();
+                                Joe.ClearBet();
+                                Bob.ClearBet();
+                                Al.ClearBet();
+                                MessageBox.Show((i + 1).ToString() + "号狗赢了");
+                                break;
+                            }                                
+                        }
+                        button1.Enabled = true;
+                        button2.Enabled = false;
+                        break;
+                    }
+                   
+                }
+            }
+        }
+        public int compare(int a, int b, int c, int d)
+        {
+            int e;
+            e = Math.Max(a, b);
+            e = Math.Max(e, c);
+            e = Math.Max(e, d);
+            return e;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Joe.mybet = Joe_b;
+            Bob.mybet = Bob_b;
+            Al.mybet = Al_b;
+            if (radioButton1.Checked)
+            {               
+                Joe_b.Amount = (int)numericUpDown1.Value; Joe_b.dog = (int)numericUpDown2.Value;
+                Joe.cash -= Joe_b.Amount;
+                label2.Text = Joe.name;
+                Joe.updatelable();
+            }
+            else if (radioButton2.Checked)
+            {                
+                Bob_b.Amount = (int)numericUpDown1.Value; Bob_b.dog = (int)numericUpDown2.Value;
+                Bob.cash -= Bob_b.Amount;
+                label2.Text = Bob.name;
+                Bob.updatelable();
+            }
+            else
+            {
+                Al_b.Amount = (int)numericUpDown1.Value; Al_b.dog = (int)numericUpDown2.Value;
+                Al.cash -= Al_b.Amount;
+                label2.Text = Al.name;
+                Al.updatelable();
+            }
+                button2.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Joe.mylabel = label5;
+            Joe.myradiobutton = radioButton1;
+            Bob.mylabel = label6;
+            Bob.myradiobutton = radioButton2;
+            Al.mylabel = label7;
+            Al.myradiobutton = radioButton3;
+        }
+    }
+    public class Greyhound
+        {
+            //public int startingposition;
+            public PictureBox mypicturebox;
+            public int Location;//25,472
+            public Random Randomizer;
+            public Greyhound(PictureBox picture, int start)
+            {
+                mypicturebox = picture;
+                Location = start;
+            }
+            public bool Run()
+            {
+            Randomizer = new Random();
+            Point p = mypicturebox.Location;
+            p.X += Randomizer.Next(20);
+            mypicturebox.Location = p;
+            Location = p.X;
+            if (mypicturebox.Location.X >= 472)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    public class Bet
+    {
+        public int Amount = 0;
+        public int dog = 0;
+        public Guy guy;
+        public Bet(Guy guy)
+        {
+            this.guy = guy;
+        }
+        public string GetDescription()
+        {
+            if (Amount == 0)
+                return "没有投注";
+            else
+            {
+                return guy.name + "投注了" + dog.ToString() + "号" + Amount.ToString() + "元";
+            }
+        }
+        public int payout(int winner)
+        {
+            if (winner == dog)
+            {
+                return 2 * Amount;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    public class Guy
+    {
+        public string name;
+        public Bet mybet;
+        public int cash;
+        public RadioButton myradiobutton;
+        public Label mylabel;
+        public Guy(string name, int cash)
+        {
+            this.name = name;
+            this.cash = cash;
+        }
+        public void updatelable()
+        {
+            if (mybet == null)
+            {
+                mylabel.Text = "没有投注";
+                myradiobutton.Text = name + "的余额为" + cash + "元";
+            }
+            else
+            {
+                mylabel.Text = name + "投注了" + mybet.dog.ToString() + "号" + mybet.Amount.ToString() + "元"; ;
+                myradiobutton.Text = name + "的余额为" + cash.ToString() + "元";
+            } 
+        }
+        public void ClearBet()
+        {
+            mybet = null;
+            updatelable();
+        }
+    }
+}
